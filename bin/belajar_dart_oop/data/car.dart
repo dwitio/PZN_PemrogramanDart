26357d5d54a7b28d35f38ca128a8bafe3a2087e0
dwitio <tiothebook@gmail.com>
2022-05-16 20:19:24 +0700

    4rd commit

---
+++ bin/belajar_dart_oop/data/car.dart
@@ -0,0 +1,34 @@
+class Car {
+  String name = "";
+
+  void drive(){
+
+  }
+
+  int getTier(){
+    return 0;
+  }
+}
+
+abstract class HasBrand {
+  String getBrand();
+}
+
+class Avanza implements Car, HasBrand{
+  @override
+  String name = "Avanza";
+
+  @override
+  void drive() {
+    print('Avanza is running');
+  }
+
+  @override
+  int getTier() {
+    return 4;
+  }
+
+  @override
+  String getBrand() => "Toyota";
+
+}
